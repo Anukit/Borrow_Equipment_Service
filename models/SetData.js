9282775f609f8c50de3591d6fe3b0d8763242d4b
Anukit Sornboonta <anukit084@hotmail.com>
2022-03-01 01:01:09 +0700

    update set equipment to department

--- models/SetData.js
+++ models/SetData.js
@@ -8,5 +8,16 @@ var SetData = {
       callback
     );
   },
+
+  getEquipDepart: function (data, callback) {
+    return db.query(
+      `SELECT c.id, c.rfid, c.serial_number, c.equipment_number, c.equipment_name, c.brand, c.model, c.update_at, c.create_at, c.active, a.borrow_date, a.return_date FROM reports as a 
+      JOIN department as b ON a.used_department_id = b.id
+      JOIN equipment as c ON a.equipment_id = c.id
+      WHERE a.used_department_id = ? ORDER BY a.borrow_date DESC, a.return_date DESC`,
+      [data.dpmID],
+      callback
+    );
+  },
 };
 module.exports = SetData;

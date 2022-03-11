d703fe271bd0dfa2cee6c64032d0db7d87e6c395
Anukit Sornboonta <anukit084@hotmail.com>
2022-03-11 18:29:51 +0700

    update sql code

--- models/SetData.js
+++ models/SetData.js
@@ -3,7 +3,7 @@ const db = require("../dbconnection");
 var SetData = {
   setDpmID: function (data, callback) {
     return db.query(
-      `UPDATE reports SET used_department_id = ?, update_at = ? WHERE equipment_id = (SELECT id FROM equipment WHERE rfid = ? AND active = 1) AND active = 1`,
+      `UPDATE reports SET used_department_id = ?, update_at = ? WHERE equipment_id = (SELECT id FROM equipment WHERE rfid = ? AND active = 1) AND status = 0 AND active = 1`,
       [data.dpmID, data.datetime, data.rfid],
       callback
     );
@@ -15,7 +15,7 @@ var SetData = {
       FROM reports as a 
       JOIN department as b ON a.used_department_id = b.id
       JOIN equipment as c ON a.equipment_id = c.id
-      WHERE a.used_department_id = ? ORDER BY a.update_at DESC`,
+      WHERE a.used_department_id = ? AND a.active = 1 AND b.active = 1 AND c.active = 1 ORDER BY a.update_at DESC`,
       [data.dpmID],
       callback
     );
